2f07909c31378a215d47178f856603d8a6bafb29
swapnil raut <swapnil.purandar@gmail.com>
2023-03-17 22:25:03 +0530

    03-refactor: Change folder & file extention

--- session14.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# Module 13: Azure Databricks Part 3
-## Session 14: Ways to mount storage
-3 Ways to access storage account:  
-1. Acces Key / Account Key  --> Access to your complete storage account (not at good for production scenarios)
-2. SAS Key: Shared access signature --> container level access
-3. Service Principal: Fine grain control till folder level
