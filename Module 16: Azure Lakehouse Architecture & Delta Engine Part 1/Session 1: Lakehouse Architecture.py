9f4388ef7e8d18266f9465a205d385c5413a3392
swapnilraut3 <swapnilraut3>
2023-09-25 11:00:18 +0000

    08-feat: Add Module16 Lakehouse Architecture and Delta Engine Part1

---
+++ Module 16: Azure Lakehouse Architecture & Delta Engine Part 1/Session 1: Lakehouse Architecture.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Session 1: Azure Lakehouse Architecture
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Hi this is the first line
+
+# COMMAND ----------
+
+
